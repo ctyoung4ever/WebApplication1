5faa9f259fce3e2ac4c6193843faceaee79e6728
ctyoung4ever <79294074+ctyoung4ever@users.noreply.github.com>
2023-02-08 01:41:20 -0700

    new functionality. Made into an MVC

---
+++ WebApplication1/Controllers/BlahController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4_collin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_collin.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Form(formmodel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/Models/formmodel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4_collin.Models
+{
+    public class formmodel
+    {
+        [Required]
+        [Range( 0, 100)]
+        public int assignments { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int groupProject { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int quizzes { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int midtermExam { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int finalExam { get; set; }
+
+        [Required]
+        [Range(0, 100)]
+        public int intex { get; set; }
+
+    }
+}
